5718183dfe2339dca9acfd827727fa4e1d6d7c23
Mirza Bilal <mirzabilal@Mirzas-MacBook-Pro.local>
2024-08-15 10:58:15 +0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import { useEffect } from "react";
+import "./index.css";
+
+export const V3mail = () => {
+  // TODO: Load up Rive File
+
+ return (
+    <RiveComponent/>
+  );
+};
+
+export default function App() {
+  return (
+    <div className="h-screen flex items-center justify-center">
+      <div className="RiveContainer">
+        <TwoAV3 />
+      </div>
+    </div>
+  );
+}
